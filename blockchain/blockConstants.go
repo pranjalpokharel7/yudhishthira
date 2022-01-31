5e6be5999163350bacea83d20a46621498ef17b0
Pranjal Pokharel <pranjalpokharel007@gmail.com>
2022-01-31 13:51:15 +0545

    Blockchain methods added (#2)
    See PR description.

--- blockchain/blockConstants.go
+++ blockchain/blockConstants.go
@@ -1,5 +1,7 @@
 package blockchain
 
+import "crypto/sha256"
+
 // all blockchain related constants go here
 type CHAIN_TYPE int
 
@@ -10,3 +12,5 @@ const (
 )
 
 const TIMESTAMP_SLICE_SIZE = 16 // in case we use fixed size slices to represent timestamps instead of strings to save space
+const HASH_SIZE = sha256.Size
+const GENESIS_STRING = "Genesis Block"
--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,14 +1,41 @@
 package blockchain
 
+import (
+	"errors"
+)
+
 // technically block chain is just a chain of blocks
 // a single int field to determine whether the chain is main chain or test chain
 type BlockChain struct {
 	chainType CHAIN_TYPE
-	Blocks    []Block
+	Blocks    []Block // store blocks or references to blocks?
 }
 
-// only has the main chain for now
-func createGenesisBlock() {}
+func (blockchain *BlockChain) AddToBlockchain(block *Block) error {
+	// maybe not create block here?
+	if len(blockchain.Blocks) == 0 {
+		return errors.New("Genesis block not created!")
+	}
+	previousBlock := blockchain.Blocks[len(blockchain.Blocks)-1]
+	block.LinkPreviousHash(&previousBlock)
+	block.CalculateHash()
+	blockchain.Blocks = append(blockchain.Blocks, *block)
+	return nil
+}
+
+func (blockchain *BlockChain) PrintChain() {
+	for _, value := range blockchain.Blocks {
+		value.PrintBlock()
+	}
+}
+
+func (blockchain *BlockChain) AddGenesisBlock(genesisBlock *Block) error {
+	if len(blockchain.Blocks) != 0 {
+		return errors.New("Genesis block already added to the chain!")
+	}
+	blockchain.Blocks = append(blockchain.Blocks, *genesisBlock)
+	return nil
+}
 
 // load block chain from JSON format
 func loadBlockChain() {}
