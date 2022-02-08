19c6b419797595e9765f32f6a8f439426547aebf
Pranjal Pokharel <pranjalpokharel007@gmail.com>
2022-02-08 14:42:32 +0545

    Init wallet branch

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,4 +1,23 @@
 package wallet
 
-// here goes the code to view the wallet
-// or in other words, the user interface is defined here to view the blockchain
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+// might use elliptic curve cryptography if this ends up becoming too slow
+type Wallet struct {
+	privateKey *rsa.PrivateKey
+	PublicKey  rsa.PublicKey
+}
+
+func (wallet *Wallet) generateKeyPair() error {
+	privateKey, err := rsa.GenerateKey(rand.Reader, RSA_KEY_SIZE)
+	if err != nil {
+		return err
+	}
+
+	wallet.privateKey = privateKey
+	wallet.PublicKey = privateKey.PublicKey
+	return nil
+}
---
+++ wallet/walletConstants.go
@@ -0,0 +1,5 @@
+package wallet
+
+// nobody uses 1024 length RSA keys
+// increase to 4096 if felt less secure
+const RSA_KEY_SIZE = 2048
