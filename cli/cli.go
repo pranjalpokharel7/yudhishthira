d81609d7786395fcacb5b2129ddd20c6444da1e2
Pranjal Pokharel <pranjalpokharel007@gmail.com>
2022-02-16 16:48:35 +0545

    Object props (#9)
    * Object Data Definition
    
    Tried to define object data in terms of CSV files. Compare hashes to prevent modification. Requires further work and tidying up.
    
    * Object Hash CLI
    
    CLI to hash object data.

--- cli/cli.go
+++ cli/cli.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pranjalpokharel7/yudhishthira/object"
+	"github.com/pranjalpokharel7/yudhishthira/utility"
 	"github.com/pranjalpokharel7/yudhishthira/wallet"
 )
 
 func CommandLineHelp() {
 	fmt.Println("Available Commands:")
-	fmt.Println("\tgenwallet --file filename - Generate wallet and store in filename")
+	fmt.Println("\t genwallet --file filename - Generate wallet and store in filename")
+	fmt.Println("\t objhash --obj filename - Generate object hash for object details stored in filename (csv for now, see ./object/dummy_object.csv)")
 }
 
 func RunCLI() {
@@ -20,17 +23,28 @@ func RunCLI() {
 	}
 
 	genWallet := flag.NewFlagSet("genwallet", flag.ExitOnError)
-	walletFileAddress := genWallet.String("file", wallet.WALLET_FILE, "The location to store the wallet file")
+	checkObjectHash := flag.NewFlagSet("objhash", flag.ExitOnError)
+
+	walletFileLocation := genWallet.String("file", wallet.WALLET_FILE, "The location to store the wallet file")
+	objectFileLocation := checkObjectHash.String("obj", "", "The location of the file where the object data is stored")
 
 	switch os.Args[1] {
 	case "genwallet":
 		err := genWallet.Parse(os.Args[2:])
-		if err != nil {
-			panic(err)
-		}
+		utility.ErrThenPanic(err)
+	case "objhash":
+		err := checkObjectHash.Parse(os.Args[2:])
+		utility.ErrThenPanic(err)
 	}
 
 	if genWallet.Parsed() {
-		wallet.GenerateWallet(*walletFileAddress)
+		wallet.GenerateWallet(*walletFileLocation)
+	}
+
+	if checkObjectHash.Parsed() {
+		var obj object.Object
+		err := obj.LoadCSVData(*objectFileLocation)
+		utility.ErrThenPanic(err)
+		fmt.Printf("Object Hash: %x\n", obj.HashObject())
 	}
 }
