3e1bba91f19258d76a63c23b272c78529a3c3a96
Sandace11 <075bct075.sandesh@pcampus.edu.np>
2022-03-13 10:52:51 +0545

    Added api call to latestBlock table

--- api/api.go
+++ api/api.go
@@ -114,9 +114,25 @@ func PostCoinbaseTransaction(walletAddress string) {
 
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 func StartServer(chain *blockchain.BlockChain) {
 	router := gin.Default()
 
+	router.Use(CORSMiddleware())
 	// block endpoint
 	router.GET("/block/last", GetLastBlockResponse(chain))
 	router.GET("/block/last/:n", GetLastNBlocksResponse(chain))
--- frontend/Visualizer/latestBlock.js
+++ frontend/Visualizer/latestBlock.js
@@ -1,32 +1,42 @@
-const latestBlockDatas = [
-    {
-        id: "blockRow_0",
-        blockHeight: 1234,
-        blockTransactions: 714,
-        hash: "000000000000000000051fbcc30db5d4f09eabe02c9a461e2f9aa9c848a8c8f0",
-        blockReward: "6.25",
-        date: new Date('2022-09-03T03:00:48')
-    }, {
-        id: "blockRow_1",
-        blockHeight: 342,
-        blockTransactions: 1253,
-        hash: "000000000000000000051fbcc30db5dasdxcqwe461e2f9aa9cszxcv848a8c8f0",
-        blockReward: "2.5",
-        date: new Date('2022-09-13T05:01:24')
-    }
-];
+
+const fetchPromise = fetch('http://localhost:8080/block/last/10');
+
+fetchPromise
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(json => {
+        let latestBlockDatas = json;
+
+        let tblBody = document.getElementById("latestBlockTable");
+        latestBlockDatas.forEach(bData => {
+            let row = document.createElement("tr");
+            row.setAttribute("id", bData.id);
+            row.innerHTML = `
+                <td><a href="#" class="blockHeight">${bData.height}</a></td>
+                <td><div class="blockTransactions">${bData.nonce}</div></td>
+                <td><a href="#" class="hashTd">${bData.block_hash}</a></td>
+                <td><b class="blockReward">${bData.difficulty}</b></td>
+                <td><div class="dateString">${bData.timestamp}</div></td>
+            `;
+            tblBody.append(row);
+        });
+    });
+
+
+// const URL = 'http://localhost:8080/block/last/3';
+
+// async function getData() {
+//     const response = await fetch(this.URI);
+//     const data = await response.json();
+//     return data;
+// }
+
+// console.log(getData())
 
 //LatestBlock
-let tblBody = document.getElementById("latestBlockTable");
-latestBlockDatas.forEach(bData => {
-    let row = document.createElement("tr");
-    row.setAttribute("id", bData.id);
-    row.innerHTML = `
-        <td><a href="#" class="blockHeight">${bData.blockHeight}</a></td>
-        <td><div class="blockTransactions">${bData.blockTransactions}</div></td>
-        <td><a href="#" class="hashTd">${bData.hash}</a></td>
-        <td><b class="blockReward">${bData.blockReward}</b></td>
-        <td><div class="dateString">${bData.date.toLocaleString()}</div></td>
-    `;
-    tblBody.append(row);
-});
+
+{/* <td><div class="dateString">${bData.date.toLocaleString()}</div></td> */ }
