064c8a4dad79931eb8a74ffea75f47b58011f474
Sandace11 <075bct075.sandesh@pcampus.edu.np>
2022-03-13 16:08:11 +0545

    API integrated in mined transactions

--- frontend/Visualizer/minedTransactions.js
+++ frontend/Visualizer/minedTransactions.js
@@ -16,38 +16,54 @@ const latestMinedTransactions = [
     }
 ];
 
+const fetchPromise = fetch('http://localhost:8080/transaction/last/10');
+
+fetchPromise
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(json => {
+        let latestMinedTransactions = json;
+
+        let tblBody2 = document.getElementById("minedTransactions");
+        latestMinedTransactions.forEach(bData => {
+            let row = document.createElement("tr");
+            let subRow = document.createElement("tr");
+            row.setAttribute("id", bData.id);
+            row.setAttribute("class", "row");
+            subRow.setAttribute("class", `${bData.id}_expand expandContent`)
+            row.innerHTML = `
+                <td><a href="#" class="txId">${bData.txID}</a></td>
+                <td><a href="#" class="txId">${bData.itemHash}</a></td>
+                <td class="amountTransacted"><b class="">${bData.amount}</b><br></td>
+                <td><div class="dateString">${bData.timestamp}</div></td>
+            `;
+            subRow.innerHTML = `
+                <td colspan="4">
+                    <div class="expandFlex">
+                        <div class="fromHolder">
+                            <b>From</b>
+                            <a href="#">${bData.sellerHash}</a>
+                        </div>
+                        <svg class="arrow" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
+                        </svg>
+                        <div class="toHolder">
+                            <b>To</b>
+                            <a href="#">${bData.buyerHash}</a>
+                        </div>
+                    </div>
+                </td>
+            `;
+            tblBody2.append(row);
+            tblBody2.append(subRow);
+        });
+    });
+
 //latestMinedTransactions
-let tblBody2 = document.getElementById("minedTransactions");
-latestMinedTransactions.forEach(bData => {
-    let row = document.createElement("tr");
-    let subRow = document.createElement("tr");
-    row.setAttribute("id", bData.id);
-    row.setAttribute("class", "row");
-    subRow.setAttribute("class", `${bData.id}_expand expandContent`)
-    row.innerHTML = `
-        <td><a href="#" class="txId">${bData.hash}</a></td>
-        <td class="amountTransacted"><b class="">${bData.amountTransacted}</b><br></td>
-        <td><div class="dateString">${bData.date.toLocaleString()}</div></td>
-    `;
-    subRow.innerHTML = `
-        <td colspan="4">
-            <div class="expandFlex">
-                <div class="fromHolder">
-                    <b>From</b>
-                    <a href="#">${bData.from}</a>
-                </div>
-                <svg class="arrow" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                    <path d="M0 0h24v24H0V0z" fill="none" />
-                    <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
-                </svg>
-                <div class="toHolder">
-                    <b>To</b>
-                    <a href="#">${bData.to}</a>
-                </div>
-            </div>
-        </td>
-    `;
-    tblBody2.append(row);
-    tblBody2.append(subRow);
-});
+
 
